8e8c58cf67eb5e2276dbee9c5e3a545ed753fe97
Garg, Anubhav <anubhav.d.garg@accenture.com>
2023-05-08 14:25:20 +0530

    server setup

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+// Serve static files....
+app.use(express.static(__dirname + '/dist/realtime-firebase'));
+
+// Send all requests to index.html
+app.get('/*', function(req, res) {
+  res.sendFile(path.join(__dirname + '/dist/realtime-firebase/index.html'));
+});
+
+// default PORT
+app.listen(process.env.PORT || 4000);
+
